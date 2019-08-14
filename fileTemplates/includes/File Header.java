a67dac581fb2ca441ff8f8f039974c5431ab8eee
lilongji <841318078@qq.com>
2019-08-14 15:40:05 +0800

    IU-2019.2 <Administrator@PC-20190404WKUY Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, project.default.xml, Default copy.xml, lombok-plugin.xml, Solarized.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, File Header.java, textmate_os.xml, filetypes.xml, projectView.xml, keymap.xml, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, java.xml, notifications.xml, databaseDrivers.xml, github.xml, mavenVersion.xml, templates.xml, Sublime Monokai True.icls, Obsidian.icls, Sublime Text 2.icls, Ergo.icls, Default.xml, activate-power-mode_setting.xml, laf.xml, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, intentionSettings.xml, web-browsers.xml, vcs.xml, codota.xml, editor.codeinsight.xml, grepConsole.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+/**
+ * @Desc:
+ * @CurProject: ${PROJECT_NAME}
+ * @Author: leesin
+ * @Email: 841318078@qq.com
+ * @Date:                                                                                                               ${DATE} ${TIME}
+ * @Version 1.0
+ * @Describe: 
+ */
